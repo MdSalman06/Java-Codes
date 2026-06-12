6d9bc57758cab632fd5b55597ac987f0f2ae9c8e
MdSalman06 <salman.reach06@gmail.com>
2026-06-12 09:31:13 +0530

    Create Anagram.java

---
+++ String/Anagram.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first string: ");
+        String str1 = sc.nextLine();
+
+        System.out.print("Enter second string: ");
+        String str2 = sc.nextLine();
+
+        str1 = str1.toLowerCase().replaceAll("\\s", "");
+        str2 = str2.toLowerCase().replaceAll("\\s", "");
+
+        char arr1[] = str1.toCharArray();
+        char arr2[] = str2.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        if (Arrays.equals(arr1, arr2))
+            System.out.println("Anagram");
+        else
+            System.out.println("Not Anagram");
+    }
+}
