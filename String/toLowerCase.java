44979bf9a6b153f168b7c293099f0173c582219a
MdSalman06 <salman.reach06@gmail.com>
2026-06-12 09:50:26 +0530

    Create toLowerCase.java

---
+++ String/toLowerCase.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main{
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a string :");
+        String str = sc.nextLine();
+        
+        String Lowercase = str.toLowerCase();
+        System.out.println("Converted String :"+Lowercase);
+    }
+}
