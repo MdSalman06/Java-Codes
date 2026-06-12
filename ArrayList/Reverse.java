2541492c385cd49eeb51203be3ec2a8f43a93588
MdSalman06 <salman.reach06@gmail.com>
2026-06-12 10:32:13 +0530

    Create Reverse.java

---
+++ ArrayList/Reverse.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Reverse{
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        Collections.reverse(list);
+        System.out.println(list);
+    }
+}
